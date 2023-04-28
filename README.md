18121e5d8e5784015e4c1c4205c5bd175a53658b
Eric Moritsuka <eric.moritsuka@liferay.com>
2023-04-28 10:17:44 -0300

    altering readme and test.java

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# java
+# My Java annotations  
+Here is where I will add the files with annotations I have taken during my Java course at Udemy.
---
+++ test.java
@@ -0,0 +1,21 @@
+ import java.time.LocalDate;
+ import java.time.LocalDateTime;
+
+ public class FutureDatesFunction
+
+     public Boolean apply(Object object1) {
+ 
+         LocalDate localDate = DateParameterUtil.getLocalDate(
+             object1.toString());
+         LocalDateTime currentDate = LocalDateTime.now();
+         System.out.println(currentDate);
+ 
+ //		if (localDate.isBefore(currentDate)) {
+ 
+             return false;
+         }
+ 
+         return true;
+     }
+ 
+ }
