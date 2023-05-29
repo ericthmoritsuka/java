aad5beb59103cd5ea083d1c193ee67bc14139ed7
Eric Moritsuka <ericthmoritsuka@gmail.com>
2023-05-29 10:16:45 -0300

    updating MinAndMaxChallenge

--- Section6/src/MinAndMaxChallenge.java
+++ Section6/src/MinAndMaxChallenge.java
@@ -5,6 +5,7 @@ public class MinAndMaxChallenge {
     public static void main(String[] args) {
         int min = Integer.MAX_VALUE;
         int max = Integer.MIN_VALUE;
+        int count = 0;
 
         while (true) {
             Scanner scanner = new Scanner(System.in);
@@ -17,10 +18,15 @@ public class MinAndMaxChallenge {
                 if (number > max){
                     max = number;
                 }
+                count += 1;
+                System.out.println("You entered " + count + " numbers so far");
                 System.out.println("The minimum number is: " + min);
                 System.out.println("The maximum number is: " + max);
             } catch (NumberFormatException badInput) {
                 System.out.println("Not a number, ending the program");
+                System.out.println("You entered " + count + " numbers");
+                System.out.println("The smallest number was: " + min);
+                System.out.println("The largest number was: " + max);
                 break;
             }
         }
